d898e6469d6facf2994c2b777a152b54e328bbe4
challenge <challenge@empire.se.rit.edu>
2015-02-05 01:20:11 -0500

    Adding Facebook SDK to header file

---
+++ Snowman/Snowman/Bridge-Header.h
@@ -0,0 +1,14 @@
+//
+//  Bridge-Header.h
+//  Snowman
+//
+//  Created by challenge on 2/5/15.
+//  Copyright (c) 2015 com.thesyedahmed. All rights reserved.
+//
+
+#ifndef Snowman_Bridge_Header_h
+#define Snowman_Bridge_Header_h
+
+#import <FacebookSDK/FacebookSDK.h>
+
+#endif
